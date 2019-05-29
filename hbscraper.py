7f5addce0e2f14ebc2dc2da04f25967ced626c60
aubinh <aubinh92@gmail.com>
2019-05-29 18:51:39 -0400

    proxies added and headers added

--- hbscraper.py
+++ hbscraper.py
@@ -6,6 +6,65 @@ import csv
 
 from bs4 import BeautifulSoup
 
+import random
+
+from itertools import cycle
+
+user_agent_list = [
+    # Chrome
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    # Firefox
+    'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)'
+]
+
+from lxml.html import fromstring
+user_agent = random.choice(user_agent_list)
+#Set the headers
+headers = {'User-Agent': user_agent}
+
+
+def get_proxies():
+    url = 'https://free-proxy-list.net/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    table=soup.find('table')
+    table_body = table.find('tbody')
+    rows = table_body.find_all('tr')
+    proxies = []
+    for row in rows:
+        cols=row.find('td')
+        for item in row:
+            try:
+                #print(int(item.text))
+                cols = str(cols.text)+":"+str(int(item.text))
+                proxies.append(cols)
+            except:
+                pass
+
+    return proxies
+
+
 
 
 
@@ -17,24 +76,31 @@ def amazon_price(query):
 
 
 def book_check(query):
-    new_url="https://www.booksprice.com/compare.do?inputData="
-    new_url += urllib.parse.quote_plus(query)
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.text, 'html.parser')
-    for link in soup.find_all('a'):
-        if(len(link.get('href'))==10):
-#            print(link)
-            value=link.get('href')
+    url = "https://www.amazon.com/s?k="
+    url += urllib.parse.quote_plus(query)
+    proxies = get_proxies()
+    #print(proxies)
+    proxy_pool = cycle(proxies)
+    for i in range(1, 11):
+        # Get a proxy from the pool
+        proxy = next(proxy_pool)
+        print("Request #%d" % i)
+        try:
+            resp = requests.get(url, proxies={"http": proxy, "https": proxy}, headers=headers)
+            soup = BeautifulSoup(resp.text, 'html.parser')
+            rows=soup.find_all("span", class_='a-offscreen')
+            otherrows=soup.find_all('span')
+            moredivs=soup.find_all('div')
+            print(moredivs)
+            #print(soup)
+            print("**************************************************")
+            print(rows)
+            print(otherrows)
             break
-#    print(value)
-    new_url="https://www.booksprice.com/comparePrice.do?l=y&searchType=compare&inputData="
-    new_url +=value
-    new_url += "&dontShowRentals=true&fromPrice=true"
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.text, 'html.parser')
-    time.sleep(5)
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.text, 'html.parser')
+        except:
+            print("Skipping. Connnection error")
+
+
 
 
 url = "https://www.humblebundle.com/"
@@ -111,6 +177,7 @@ for url in list_of_urls:
         for product in tierinfo['products']:
             print(product)
             print(product_dict[product])
+            print(book_check(product))
             csv_dict[product] = {
                 "Book": product,
                 "Bundle":list_of_bundles[counter-1] ,
